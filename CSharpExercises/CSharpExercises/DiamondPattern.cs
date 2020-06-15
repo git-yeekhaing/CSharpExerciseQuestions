3bc14ed558d70f5cb9384ceedcff64a9039d150c
git-yeekhaing <yee.khaing.git@gmail.com>
2020-06-15 18:11:38 +0900

    diamond pattern1

---
+++ CSharpExercises/CSharpExercises/DiamondPattern.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    public class DiamondPattern
+    {
+        public static void Pattern1()
+        {
+            Console.WriteLine("input max stars sizes.");
+            int maxStar = Convert.ToInt32(Console.ReadLine());
+
+            int starCnt = 1;
+            // upper loop
+            for (int row = 1; row <= maxStar; row++)
+            {
+                // print space
+                for (int space = 1; space < (maxStar - row) + 1; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                // print stars
+                for (int star = 1; star <= starCnt; star++)
+                {
+                    Console.Write("*");
+                }
+
+                starCnt += 2;
+
+                Console.WriteLine();
+            }
+
+            // below loop
+            starCnt = starCnt - 4; int belowSpace = 1;
+
+            // upper loop
+            for (int row = 1; row <= maxStar; row++)
+            {
+                // print space
+                for (int space = 1; space <= belowSpace; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                // print stars
+                for (int star = 1; star <= starCnt; star++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+                starCnt -= 2;
+                belowSpace += 1;
+            }
+        }
+    }
+}
--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -10,7 +10,7 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number.");
+           /* Console.WriteLine("Enter the number.");
             int input = Convert.ToInt32(Console.ReadLine());
             bool result = HarshadNumber.IsHarshadNumber(input);
 
@@ -19,6 +19,9 @@ namespace CSharpExercises
 
             // 2nd methods
             HarshadNumber.checkForHarshad(input);
+           */
+            // diamond pattern1
+            DiamondPattern.Pattern1();
             Console.ReadLine();
         }
     }
