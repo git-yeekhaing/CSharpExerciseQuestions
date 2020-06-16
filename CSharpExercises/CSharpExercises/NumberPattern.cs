1b5e6b8a11ca2cb500c0195e6c1f3c26b1c19a8e
git-yeekhaing <yee.khaing.git@gmail.com>
2020-06-16 13:56:41 +0900

    add number pattern1

---
+++ CSharpExercises/CSharpExercises/NumberPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    public class NumberPattern
+    {
+        public static void Pattern1()
+        {
+            Console.WriteLine("Enter size");
+            int rowCnt = Convert.ToInt32(Console.ReadLine());
+
+            int startOdd = 1;
+            for (int row = 1; row <= rowCnt; row++)
+            {
+                // print space
+                for (int space = 1; space <= (rowCnt - row) + 1; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                // for number
+                bool bCenter = false; int outputNum = 1;
+                for(int num = 1; num <= startOdd; num++)
+                {
+                    Console.Write(outputNum);
+                    if (outputNum == row)
+                    {
+                        bCenter = true;
+                    }
+
+                    outputNum = (bCenter) ? --outputNum : ++outputNum;
+                }
+
+                startOdd += 2;
+                Console.WriteLine();
+            }            
+        }
+    }
+}
--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -10,18 +10,8 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-           /* Console.WriteLine("Enter the number.");
-            int input = Convert.ToInt32(Console.ReadLine());
-            bool result = HarshadNumber.IsHarshadNumber(input);
 
-            if (result == true)
-              Console.WriteLine($"{input} is a Harshad number");
-
-            // 2nd methods
-            HarshadNumber.checkForHarshad(input);
-           */
-            // diamond pattern1
-            DiamondPattern.Pattern3();
+            NumberPattern.Pattern1();
             Console.ReadLine();
         }
     }
